4a5d188fba819b4f779b920372ea3edc75efe9c4
Alex Nguyen <ana58@sfu.ca>
2024-04-28 17:48:34 -0700

    implement hook detection

--- Hook.gd
+++ Hook.gd
@@ -1,97 +1,32 @@
 extends Area2D
 
-enum STATE {INACTIVE, ACTIVE, HOOKED, RETURN}
-var Rope = preload("res://Rope.tscn")
+var velocity
+var ang
+var duration
 
-var velocity = Vector2(1,1) * 500
-var ang = 2*PI
-var hook_state = STATE.INACTIVE
-var active_hook_duration = 0
+signal hit_detected(hit_location)
 
-##### TO DO: ######   
-# Implement player movement during hooked state.
-##### ------ ######
+### Possible Problems ###
+# Detection is made by a traveling body. If that body is moving too fast it can skip areas
+# it should detect. Ideally want to be able to detect a hit instantly but can not happen in
+# this implementaion. 
+# Best to try to find a new implementation that is in constant detection.
+###
 
 func _ready():
-	get_node("../Player").connect("hook_launch", self, "shoot")
-	get_node("../Player").connect("release_signal", self, "release")
+	var player = get_node("../PlayerChar")
+	velocity = Vector2(1,1) * 1000
+	ang = PI*5/8
+	duration = 10
 
 func _process(delta):
-	var direction = Vector2()
-	var max_hook_duration = 40
-	match hook_state:
-		STATE.INACTIVE:
-			pass
-		STATE.HOOKED:
-			pass
-		STATE.ACTIVE: 
-			direction.x = velocity.x * cos(ang)
-			direction.y = velocity.y * sin(ang)
-			position += direction * delta
-			
-			active_hook_duration += 1
-			if active_hook_duration >= max_hook_duration:
-				self.switch_to_return()
-		STATE.RETURN:
-			var player = get_node("../Player")
-			ang = position.angle_to_point(player.position)
-			
-			set_global_rotation(ang)
-			direction.x = velocity.x * cos(ang)
-			direction.y = velocity.y * sin(ang)
-			position -= direction*delta
+	if(duration == 0):
+		queue_free()
+	var direction = Vector2(1, 1)
+	direction.x = velocity.x * cos(ang)
+	direction.y = velocity.y * sin(ang)
+	position -= direction * delta
+	duration -= 1
 
-func shoot(mouse_pos, player_pos):
-### Lauch the hook in the mouse direction ###
-	if(hook_state == STATE.HOOKED):
-		delete_rope()
-		self.switch_to_inactive()
-	elif(hook_state == STATE.INACTIVE):
-		self.switch_to_active()
-		ang = mouse_pos.angle_to_point(player_pos)
-		set_global_position(player_pos)
-		set_global_rotation(ang)
-		active_hook_duration = 0
-
-func _on_Hook_body_entered(body):
-### When the flying hook returns to the player's location ###
-	if(body == get_node("../Player") && hook_state == STATE.RETURN):
-		self.switch_to_inactive()
-	if(body != get_node("../Player") && hook_state == STATE.ACTIVE):
-		self.switch_to_hooked()
-
-func release():
-### unhook the hook ###
-	if(hook_state == STATE.HOOKED):
-		self.switch_to_inactive()
-
-func switch_to_inactive():
-	visible = false
-	hook_state = STATE.INACTIVE
-
-func switch_to_active():
-	visible = true
-	hook_state = STATE.ACTIVE
-
-func switch_to_hooked():
-	hook_state = STATE.HOOKED
-	generate_rope()
-
-func switch_to_return():
-	hook_state = STATE.RETURN
-	
-func generate_rope():
-### Creates a new rope in the scene ###
-	var hook_start_pos = get_node("../Player").global_position
-	var hook_end_pos = get_node("../Hook").global_position
-
-	var rope = Rope.instance()
-	get_node("..").add_child(rope)
-	rope.spawn_rope(hook_start_pos, hook_end_pos)
-	
-func delete_rope():
-### Removes the rope from the scene ###
-	get_node("../Rope").queue_free()
-
-func is_hooked():
-	return hook_state == STATE.HOOKED
+func _on_Hook_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	emit_signal("hit_detected", global_position)
--- PlayerChar.gd
+++ PlayerChar.gd
@@ -1,16 +1,29 @@
 extends KinematicBody2D
 
+var Hook = preload("res://Hook.tscn")
 var player_state
-var velocity = Vector2(0, 0)
-var speed = 20
-var player_direction = 0
-var dash_timer = 0
-var dash_cd = 1
+var hook
+var velocity : Vector2 
+var speed : int
+var player_direction : int
+var dash_timer : int
+var dash_cd : int
+var animation_frame_remainder : int
 
-enum STATE {INAIR, ONGROUND}
+# TEMP
+var hook_line : Vector2
+
+enum STATE {INAIR, ONGROUND, HOOKED}
 
 func _ready():
-	pass
+	speed = 20
+
+func _draw():
+	var plots = []
+	if Input.is_action_pressed("mouse_left_press"):
+		plots.append(Vector2.ZERO)
+		plots.append(hook_line)
+	draw_polyline(plots, Color.black)
 
 func _process(delta):
 	# Horizontal Movement
@@ -28,7 +41,23 @@ func _process(delta):
 		dash_cd -= 1
 		dash_timer = 5
 	
+	if Input.is_action_just_pressed("mouse_left_press"):
+		hook_line.x = 100*sin(PI*7/8)
+		hook_line.y = 100*cos(PI*7/8)
+		
+		hook = Hook.instance()
+		add_child(hook)
+		get_node("Hook").connect("hit_detected", self, "_hook_hit_detected")
+		animation_frame_remainder = 15
+	
+	if animation_frame_remainder > 0:
+		print(animation_frame_remainder)
+		animation_frame_remainder -= 1
+		in_animation()
+	
+	
 	move_and_slide(velocity, Vector2.UP)
+	update()
 	
 	if dash_timer > 0:
 		dash_timer -= 1
@@ -42,12 +71,14 @@ func _process(delta):
 		velocity.y = 0
 	else:
 		in_air()
-		
+
 func on_ground():
 	player_state = STATE.ONGROUND
 	dash_cd = 1
 func in_air():
 	player_state = STATE.INAIR
+func in_animation():
+	velocity = Vector2(0, 0)
 
 func left():
 	if Input.is_action_pressed("move_left"):
@@ -63,3 +94,5 @@ func dash_boost():
 func scale_speed(var i : int):
 	return i * speed
 
+func _hook_hit_detected(var hit_location):
+	print(hit_location)
